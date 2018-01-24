853fe0c00ce99f0e93dc5eeb5f89361f3fd8ee1e
kgresmer <kevin_gresmer@optum.com>
2018-01-24 14:43:11 -0600

    interpolation for rotation!

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -5,20 +5,47 @@ class Deck extends Component {
     constructor(props) {
         super(props);
 
+        const position = new Animated.ValueXY();
+
         const panResponder = PanResponder.create({
             onStartShouldSetPanResponder: () => {},
             onPanResponderMove: (event, gesture) => {
-                console.log(gesture);
+                position.setValue({x: gesture.dx, y: gesture.dy });
             },
             onPanResponderRelease: () => {}
         });
 
         this.panResponder = panResponder;
+        this.position = position;
+    }
+
+    getCardStyle() {
+        const rotate = this.position.x.interpolate({
+            inputRange: [-500, 0, 500],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+
+        return {
+            ...this.position.getLayout(),
+            transform: [{ rotate }]
+        }
     }
 
     renderCards() {
-        return this.props.data.map(item => {
-           return this.props.renderCard(item);
+        return this.props.data.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <Animated.View
+                        key={item.id}
+                        style={this.getCardStyle()}
+                        {...this.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                );
+            }
+
+            return this.props.renderCard(item);
         });
     }
 
